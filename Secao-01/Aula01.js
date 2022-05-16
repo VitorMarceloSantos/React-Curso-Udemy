49585279e18d391c8e6d0b0baf12c1e15cc471d4
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2022-05-16 11:19:27 -0300

    Hello World

---
+++ Secao-01/Aula01.js
@@ -0,0 +1,20 @@
+/*
+React:
+  - é uma biblioteca JavaScript para desenvolvimento de aplicações front-end, estas aplicações são chamadas de SPA(Single Page Application).
+  - A arquitetura é baseada em componetes.(barra de navegação, card, navegeação, ...)
+  - Pode ser inserido em uma aplicação ou podemos criar a aplicação.
+
+  - Para nossa aplicação vamos utilizar um executor de scripts do Node, que é o npx.
+  - Com o comando, npx create-react-app<nome> temos uma nova aplicação sendo gerada. Podemos iniciar a aplicação com npm start.
+
+  - As mudanças no projeto são realizadas dentro da pasta src.
+
+  -Estrutura Base do React: há algumas pastas e arquivos chave para o desenvolvimento em React.
+  -node_modules: onde as dependências do projeto ficam.
+  -public: Asssets estáticos e HTML de inicialização.
+  src: onde vamos programar as nossas apps.
+  -src/index.js: Arquivo de inicialização do React;
+  - src/App.js: Componente principal da aplicação.
+
+
+*/
---
+++ Secao-01/hello-world/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World React !</h1>
+      <p>Estou muito feliz de começar a aprender React.</p>
+    </div>
+  );
+}
+
+export default App;
