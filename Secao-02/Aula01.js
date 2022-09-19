6b354890e05f759774b8f8b2f7d4a9f2cf0cc3c8
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2022-09-19 16:10:45 -0300

    React

--- Secao-02/Aula01.js
+++ Secao-02/Aula01.js
@@ -21,4 +21,12 @@
     - Na parte da função onde é executada a lógica, a sintaxe é: //Algum comentário;
     // - E támbem no JSX: {/* Algum comentário /}
     - As chaves nos permitem executar sentenças em JavaScript, veremos isso mais adiante.
+
+  IMAGENS NO REACT
+    - As imagens podem ficar na pasta public, e chamadas diretamente pela tag /nome.jpg(<img src="/nome.jpg" alt="Paisagem">). Pois a pasta public fica linkada diretamente com o src das imagens.
+    - Colocando as imagens dentro do src(criando uma pasta chamada assets), precisamos importar as imagens(import nomeImagem from "./endereço.jpg"), e o src da imagem é dinâmico com o nome da importação.(<img src={nomeImagem} alt="Paisagem">).
+
+  HOOKS
+    -Recursos do React que tem diversas funções, como guardar e alterar o estado de algum dado na nossa aplicação. Todos os hooks começam com use, por exemplo useState. Podemos criar nosso hook, chamando de hookCustom.
+    - Os hooks precisam ser importados.
 */
